33b76aff00b61e62dd8dac4ec3ff1b629dabc220
Jonsey <mwjones2186@gmail.com>
2022-08-02 19:45:51 -0700

    category routes 95% complete. Both GET, POST and DELETE routes work and confirmed. I believe the PUT route is ok but im struggling to test it. Need to figure out how to INSOMNIA a put request. More to come.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,43 +1,79 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    include: [Product]
-  }).then((dbCategoryData) => res.json(dbCategoryData))
+    include: [Product],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
     .catch((err) => {
       res.status(500).json(err);
-    })
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   Category.findOne({
-    where:{
-      id: req.params.id
+    where: {
+      id: req.params.id,
     },
-    include: [Product]
-  }).then((dbCategoryData) => res.json(dbCategoryData))
-  .catch((err) => {
+    include: [Product],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name,
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  // update a category by its `id` value
+  Category.put({
+    where: {
+      id: req.params.id,
+    },
+  }).then(dbCategoryData =>{
+    if (!dbCategoryData) {
+      res.status(404).json({message: 'No category found with that ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  }).catch(err =>{
     res.status(500).json(err);
   })
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+    Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbCategoryData =>{
+      if (!dbCategoryData) {
+        res.status(404).json({message: 'No category found with that ID'});
+        return;
+      }
+      res.json(dbCategoryData);
+    }).catch(err =>{
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
